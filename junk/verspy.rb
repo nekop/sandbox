3fce298e7d35457879035087b6e22c7528a1fbb3
Takayoshi Kimura <tkimura@redhat.com>
2011-08-31 11:13:28 +0900

    Add uri support, clean up and bump version to 1.0

--- junk/verspy.rb
+++ junk/verspy.rb
@@ -1,10 +1,13 @@
 #!/usr/bin/env ruby
 
+# Example command
 # ruby verspy.rb -s /path/to/jboss-seam-src/src/main -p org.jboss.seam stacktrace.log
+# ruby verspy.rb -s http://anonsvn.jboss.org/repos/seam/tags/JBPAPP_4_3_CP05_FP/src/main -p org.jboss.seam stacktrace.log
 
 require 'optparse'
+require 'open-uri'
 
-Version = "0.1"
+Version = "1.0"
 
 option_hash = {}
 OptionParser.new { |opt|
@@ -17,6 +20,8 @@ opt_source_path = option_hash[:s]
 opt_package_name = option_hash[:p]
 
 raise if opt_source_path.nil? or opt_package_name.nil?
+
+opt_source_path.chop! if opt_source_path[-1] == '/'[0] # drop last slash
 
 STACKTRACE_RE = /\s+at\s(#{opt_package_name}[^\(]+)\(([\w\.]+):(\d+)\)\s*$/
 
@@ -33,10 +38,6 @@ ARGF.each do |line|
     package_name = class_name[0...class_name.rindex('.')]
     source_file = "#{opt_source_path}/#{package_name.gsub(/\./, '/')}/#{source}"
 
-#    puts "#{class_name} #{method_name} #{source} #{source_linenum}"
-#    puts "#{package_name}"
-#    puts source_file
-
     open(source_file) do |f|
       body = f.readlines
       target_line = body[source_linenum-1]
@@ -47,6 +48,8 @@ ARGF.each do |line|
         puts "#{source_linenum}: #{body[source_linenum-1]}"
         puts "#{source_linenum+1}: #{body[source_linenum]}"
       end
-    end
+    end rescue puts "#{source_file} not found"
   end
 end
+
+puts "Done, processed #{lines_done.size} stack trace elements."
