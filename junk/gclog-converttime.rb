dc3a411e6ab299ac6d993612d6dfc185f3442d8a
Takayoshi Kimura <tkimura@redhat.com>
2013-06-17 12:08:39 +0900

    Fix todos

--- junk/gclog-converttime.rb
+++ junk/gclog-converttime.rb
@@ -1,19 +1,30 @@
 #!/usr/bin/env ruby
 
-# todo: use optparse and implement delta option
+require 'optparse'
+require 'time'
 
-delta = 0
+Version = "2.0"
+
+option_hash = {}
+OptionParser.new { |opt|
+  opt.on('-b base datetime', '--base-datetime') { |v| option_hash[:b] = v }
+  opt.parse!(ARGV)
+}
+
+base_datetime_s = option_hash[:b]
+if base_datetime_s.nil? then
+  raise ArgumentError, "-b <base datetime> option is required"
+end
+base_datetime = Time.parse(base_datetime_s)
+delta = base_datetime.to_f
 
 while gets do
   if /^(\d+)\.(\d{3}):/ =~ $_.chomp! then
-    totalsec = $1.to_i
-    totalsec += delta
-    while totalsec < 0 do totalsec = totalsec + (24 * 60 * 60) end
-    while (24 * 60 * 60) <= totalsec do totalsec = totalsec - (24 * 60 * 60) end
-    hour = totalsec / 60 / 60
-    min = totalsec / 60 % 60
-    sec = totalsec % 60
-    puts "#{sprintf("%02d", hour)}:#{sprintf("%02d", min)}:#{sprintf("%02d", sec)}.#{$2} #{$_}"
+    gc_msec = "#{$1}.#{$2}".to_f
+    gc_datetime = gc_msec + delta
+    t = Time.at(gc_datetime)
+    actual_datetime = t.strftime("%Y-%m-%d %H:%M:%S.%L")
+    puts "#{actual_datetime} #{$_}"
   else
     puts $_
   end
