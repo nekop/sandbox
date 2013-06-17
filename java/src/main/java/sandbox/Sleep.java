5a79228a875d69c3c3f679087f2aa063cc2dc3f5
Takayoshi Kimura <tkimura@redhat.com>
2013-06-17 12:07:10 +0900

    Fix wrong unit

--- java/src/main/java/sandbox/Sleep.java
+++ java/src/main/java/sandbox/Sleep.java
@@ -2,12 +2,12 @@ package sandbox;
 
 public class Sleep {
     public static void main(String[] args) throws Exception {
-        long sec = Long.MAX_VALUE;
+        long msec = Long.MAX_VALUE;
         if (args.length != 0) {
             try {
-                sec = Long.parseLong(args[0]);
+                msec = Long.parseLong(args[0]);
             } catch (Exception ignore) { }
         }
-        Thread.sleep(sec);
+        Thread.sleep(msec);
     }
 }
