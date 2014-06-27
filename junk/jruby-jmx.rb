489d424f4d43652362cc94feb2f8c5d4550b0cf2
Takayoshi Kimura <tkimura@redhat.com>
2014-06-27 17:24:59 +0900

    Add auth example

--- junk/jruby-jmx.rb
+++ junk/jruby-jmx.rb
@@ -13,6 +13,8 @@ port = 9999
 jmx_service_url = JMXServiceURL.new("service:jmx:remoting-jmx://#{host_name}:#{port}")
 puts jmx_service_url
 environment = HashMap.new
+# creds = ["username", "password"].to_java(:String)
+# environment.put(JMXConnector.CREDENTIALS, creds);
 jmx_connector = JMXConnectorFactory::connect(jmx_service_url, environment)
 conn = jmx_connector.getMBeanServerConnection()
 puts conn.getMBeanCount()
