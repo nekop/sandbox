92ee8bbe5d168ad29a5ae9c3cb7718f4baccbe4f
Takayoshi Kimura <takayoshi@gmail.com>
2015-08-05 10:22:31 +0900

    Remove old scripts

--- vagrant/ose300/destroy.sh
+++
@@ -1,7 +0,0 @@
-#/bin/bah
-
-ssh vagrant@ose300-master sudo subscription-manager unregister &
-ssh vagrant@ose300-node1 sudo subscription-manager unregister &
-ssh vagrant@ose300-node2 sudo subscription-manager unregister &
-wait
-vagrant destroy
